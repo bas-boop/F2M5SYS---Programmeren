6244b9e65905935abdf4f180f7afe9568720dea1
Bas <30810@ma-web.nl>
2021-10-15 09:47:55 +0200

    deel 2 van overnemen gebeuren

---
+++ Inheritans2/Inheritans2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inheritans2
+{
+    abstract class Equipment
+    {
+        public abstract void Equip();
+    }
+    class Weapon : Equipment
+    {
+        string Sword = ("Sword");
+        public override void Equip()
+        {
+            Console.WriteLine(Sword);
+        }
+    }
+    class Armor : Equipment
+    {
+        string Iron = ("Iron armor");
+        public override void Equip()
+        {
+            Console.WriteLine(Iron);
+        }
+    }
+}
